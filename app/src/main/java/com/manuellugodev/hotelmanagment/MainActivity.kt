b711f3e6c4e67f10f7a58e80a294e0f2dc515f37
Manuel Lugo <manuellugo2000ml@gmail.com>
2024-04-05 17:03:50 -0500

    Added Navigation Bar

--- app/src/main/java/com/manuellugodev/hotelmanagment/MainActivity.kt
+++ app/src/main/java/com/manuellugodev/hotelmanagment/MainActivity.kt
@@ -5,55 +5,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.Bed
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
-import androidx.compose.material.icons.rounded.Search
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.DateRangePicker
-import androidx.compose.material3.DateRangePickerState
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberDateRangePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.manuellugodev.hotelmanagment.Screens.ReservationScreen
 import com.manuellugodev.hotelmanagment.navigation.Navigation
+import com.manuellugodev.hotelmanagment.navigation.Screen
 import com.manuellugodev.hotelmanagment.ui.theme.HotelManagmentTheme
-import com.manuellugodev.hotelmanagment.utils.convertLongToTime
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -63,9 +43,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HotelManagmentTheme {
-                val navController= rememberNavController()
-                Scaffold(topBar = { TopBar() }, bottomBar = {BottomBar()}) { innerPadding ->
-                    Box(modifier = Modifier.padding(innerPadding)){
+                val navController = rememberNavController()
+                Scaffold(
+                    topBar = { TopBar() },
+                    bottomBar = { BottomBar(navController) }) { innerPadding ->
+                    Box(modifier = Modifier.padding(innerPadding)) {
                         Navigation(navController)
                     }
 
@@ -91,17 +73,28 @@ fun TopBar() {
 }
 
 @Composable
-fun BottomBar(){
-    BottomAppBar(
+fun BottomBar(navController: NavHostController) {
+
+    NavigationBar(
         containerColor = MaterialTheme.colorScheme.primaryContainer,
         contentColor = MaterialTheme.colorScheme.primary,
     ) {
-        Text(
-            modifier = Modifier
-                .fillMaxWidth(),
-            textAlign = TextAlign.Center,
-            text = "Bottom app bar",
-        )
+        NavigationBarItem(
+            selected = true,
+            onClick = { navController.navigate(Screen.ReservationScreen.route) },
+            icon = { Icon(Icons.Default.Home, "Home") },
+            label = { Text("Home") })
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = { Icon(Icons.Default.Bed, "Reservations") },
+            label = { Text("Reservations") })
+        NavigationBarItem(
+            selected = false,
+            onClick = { /*TODO*/ },
+            icon = { Icon(Icons.Default.Person, "Profile") },
+            label = { Text("Profile") })
+
     }
 }
 
