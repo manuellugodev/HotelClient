ef64f7fbbb563dbe096e6c2f9d9939e3d1f30942
Manuel <manuellugo2000ml@gmail.com>
2024-05-24 19:12:53 -0500

    Clean Imports

--- app/src/main/java/com/manuellugodev/hotelmanagment/features/reservations/presentation/screens/MyReservationScreen.kt
+++ app/src/main/java/com/manuellugodev/hotelmanagment/features/reservations/presentation/screens/MyReservationScreen.kt
@@ -1,23 +1,14 @@
 package com.manuellugodev.hotelmanagment.features.reservations.presentation.screens
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
+
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.manuellugodev.hotelmanagment.features.core.composables.ErrorSnackbar
 import com.manuellugodev.hotelmanagment.features.reservations.presentation.viewmodels.MyReservationsViewModel
 import com.manuellugodev.hotelmanagment.features.reservations.utils.MyReservationEvent
