5904e8888b46e45f96800f96542246eacfdd7f66
Manuel <manuellugo2000ml@gmail.com>
2024-05-28 10:57:20 -0500

    Implemented test for composable ProfileScreen

--- app/src/main/java/com/manuellugodev/hotelmanagment/features/profile/presentation/ProfileScreen.kt
+++ app/src/main/java/com/manuellugodev/hotelmanagment/features/profile/presentation/ProfileScreen.kt
@@ -28,9 +28,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.manuellugodev.hotelmanagment.features.profile.domain.Profile
 import com.manuellugodev.hotelmanagment.features.core.navigation.Screen
 import com.manuellugodev.hotelmanagment.features.core.navigation.navigateAndCleanBackStack
+import com.manuellugodev.hotelmanagment.features.profile.domain.Profile
 
 
 @Composable
@@ -69,7 +69,7 @@ fun ProfileScreen(state:ProfileState,onEvent:(ProfileEvent)->Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             if (state.showProfile!=null) {
-                DataProfile(state.showProfile!!)
+                DataProfile(state.showProfile)
             }
 
             Button(
