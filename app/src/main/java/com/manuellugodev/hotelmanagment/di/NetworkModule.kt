170b367ab9bd4a897aa2fb9ab4b794784036446e
Manuel Lugo <manuellugo2000ml@gmail.com>
2024-09-15 17:01:06 -0500

    Modify to production Url

--- app/src/main/java/com/manuellugodev/hotelmanagment/di/NetworkModule.kt
+++ app/src/main/java/com/manuellugodev/hotelmanagment/di/NetworkModule.kt
@@ -61,7 +61,7 @@ class NetworkModule {
     @Singleton
     @Named("baseUrl")
     fun baseUrlProvider() =
-        "http://10.0.2.2:8080/"// "https://app-hotel-5687665021bc.herokuapp.com/"
+        "https://app-hotel-5687665021bc.herokuapp.com/" // "http://10.0.2.2:8080/"
 
 
 }
