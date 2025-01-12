788312a052ff94d2996a7df5151b30deefd0a365
Manuel Lugo <manuellugo2000ml@gmail.com>
2025-01-11 21:05:11 -0600

    Implemented Confirmation of password and fixed visual bug with Textfield

--- app/src/main/java/com/manuellugodev/hotelmanagment/features/auth/presentation/screens/RegisterScreen.kt
+++ app/src/main/java/com/manuellugodev/hotelmanagment/features/auth/presentation/screens/RegisterScreen.kt
@@ -1,5 +1,6 @@
 package com.manuellugodev.hotelmanagment.features.auth.presentation.screens
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -16,18 +17,24 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Snackbar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.manuellugodev.hotelmanagment.R
 import com.manuellugodev.hotelmanagment.features.auth.presentation.viewmodels.RegisterViewModel
 import com.manuellugodev.hotelmanagment.features.auth.utils.RegisterEvent
 import com.manuellugodev.hotelmanagment.features.auth.utils.RegisterState
@@ -36,9 +43,9 @@ import com.manuellugodev.hotelmanagment.features.core.navigation.Screen
 
 
 @Composable
-fun RegisterScreenRoot(navController: NavController,viewModel: RegisterViewModel){
+fun RegisterScreenRoot(navController: NavController, viewModel: RegisterViewModel) {
 
-    RegisterScreen(state = viewModel.state,viewModel::onEvent){
+    RegisterScreen(state = viewModel.state, viewModel::onEvent) {
         navController.navigate(Screen.LoginScreen.route)
     }
 
@@ -48,9 +55,7 @@ fun RegisterScreenRoot(navController: NavController,viewModel: RegisterViewModel
 fun RegisterScreen(state:RegisterState,onEvent:(RegisterEvent)->Unit,onNavigateToLogin:()->Unit) {
 
 
-    if (state.navigateToLogin) {
-        onNavigateToLogin()
-    }
+
 
     Column(modifier = Modifier.padding(20.dp), horizontalAlignment = Alignment.CenterHorizontally) {
 
@@ -73,22 +78,18 @@ fun RegisterScreen(state:RegisterState,onEvent:(RegisterEvent)->Unit,onNavigateT
                 onValueChange = { onEvent(RegisterEvent.onUsernameEnter(it)) },
                 label = { Text(text = "Username") },
                 singleLine = true,
-                supportingText = {
-                    if (state.usernameError.isNotEmpty()) {
-                        Text(text = state.usernameError)
-                    }
-                },
+                supportingText = if (state.usernameError.isNotEmpty()) {
+                    { Text(text = state.usernameError) }
+                } else null,
                 isError = state.usernameError.isNotEmpty()
-
-
-                )
+            )
 
             TextField(
+                modifier = Modifier
+                    .fillMaxWidth(),
                 value = state.password,
                 label = { Text(text = "Password") },
                 onValueChange = { onEvent(RegisterEvent.onPasswordEnter(it)) },
-                modifier = Modifier
-                    .fillMaxWidth(),
                 singleLine = true,
                 isError = state.passwordError.isNotEmpty(),
                 visualTransformation = if (state.isShowingPassword) VisualTransformation.None else PasswordVisualTransformation(),
@@ -107,8 +108,26 @@ fun RegisterScreen(state:RegisterState,onEvent:(RegisterEvent)->Unit,onNavigateT
                             contentDescription = "Toggle password visibility"
                         )
                     }
-                }
+                },
+                supportingText = if (state.passwordError.isNotEmpty()) {
+                    { Text(text = state.passwordError) }
+                } else null
             )
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(imageVector = Icons.Default.Lock, Icons.Default.Lock.name)
+                },
+                value = state.confirmPassword,
+                onValueChange = { onEvent(RegisterEvent.onPasswordConfirmationEnter(it)) },
+                label = { Text(text = "Confirm Password") },
+                singleLine = true,
+                isError = state.passwordError.isNotEmpty(),
+                visualTransformation = if (state.isShowingPassword) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+
+                )
+
             TextField(
                 modifier = Modifier.fillMaxWidth(),
                 leadingIcon = { Icon(imageVector = Icons.Default.Person, contentDescription = "") },
@@ -137,13 +156,10 @@ fun RegisterScreen(state:RegisterState,onEvent:(RegisterEvent)->Unit,onNavigateT
                 label = { Text(text = "Email") },
                 singleLine = true,
                 isError = state.emailError.isNotEmpty(),
-                supportingText = {
-                    if (state.emailError.isNotEmpty()) {
-                        Text(text = "Enter a valid Email")
-                    }
-                }
-
-                )
+                supportingText = if (state.emailError.isNotEmpty()) {
+                    { Text(text = "Enter a valid Email") }
+                } else null
+            )
 
             TextField(
                 modifier = Modifier.fillMaxWidth(),
@@ -151,7 +167,11 @@ fun RegisterScreen(state:RegisterState,onEvent:(RegisterEvent)->Unit,onNavigateT
                 value = state.phone,
                 onValueChange = { onEvent(RegisterEvent.onPhoneEnter(it)) },
                 label = { Text(text = "Phone") },
-                singleLine = true
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    keyboardType = KeyboardType.Number // Set the keyboard to numeric mode
+                ),
+
             )
 
 
@@ -167,6 +187,29 @@ fun RegisterScreen(state:RegisterState,onEvent:(RegisterEvent)->Unit,onNavigateT
                 onEvent(RegisterEvent.dismissError)
             }
         }
+        if (state.navigateToLogin) {
+            Snackbar(
+                modifier = Modifier.padding(16.dp),
+                action = {
+                    TextButton(onClick = { onNavigateToLogin() }) {
+                        Text(
+                            text = "Dismiss",
+                            modifier = Modifier.padding(8.dp),
+                            if (isSystemInDarkTheme()) Color.Black else Color.White
+                        )
+                    }
+
+
+                },
+                content = {
+                    Text(
+                        text = stringResource(id = R.string.success_message_register),
+                    )
+                },
+
+                )
+        }
+
 
 
     }
